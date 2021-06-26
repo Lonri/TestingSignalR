ab4f99b3298379d79df2d07f7cdf3a1f4f046acf
Daniela Policarpo <daniela.policarpo@outlook.com>
2021-06-26 14:20:26 +0100

    Cleaned up Client.cs

--- Assets/Scripts/Client.cs
+++ Assets/Scripts/Client.cs
@@ -1,14 +1,9 @@
 using UnityEngine;
 using System;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR.Client;
 
 public class Client: MonoBehaviour
 {
-	// Declaration the variables
-
-	// SignalR variables
-	//private static ClientWebSocket _Socket;
 	private readonly Uri _Uri = new Uri("http://localhost:5000");
 	private HubConnection _Connection;
 
@@ -18,24 +13,5 @@ public class Client: MonoBehaviour
 		.WithUrl(_Uri)
 		.Build();
 		await _Connection.StartAsync();
-		// _Socket = new ClientWebSocket();
 	}
-
-	/*public async void OnClickStartConnection()
-	{
-		// await _Socket.ConnectAsync(_Uri, System.Threading.CancellationToken.None);
-	}*/
-
-	/*public async void CreateHamster() {
-        Task result = _Connection.InvokeAsync("CreateHamster");
-        await result;
-        Debug.Log("RESULT: " + result);
-    }*/
-
-
-	private interface IClient
-	{
-		Task CreateHamster();
-	}
-
 }
